1b4a548b63bae40fe60c4cd9d574d6d2ba5b3bf3
oreland97@gmail.com <123698741a>
2018-09-15 13:32:31 +0300

    Initial commit

---
+++ src/main/java/com/oreland/myevents/MyEventsApplication.java
@@ -0,0 +1,12 @@
+package com.oreland.myevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEventsApplication.class, args);
+    }
+}
